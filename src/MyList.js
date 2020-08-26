edb08d47a5278c71a2b651dfb4182817bbf80df9
Lizz <lizwesterband@gmail.com>
2020-08-26 15:46:12 -0700

    [part2] Add completed part 2

--- src/MyList.js
+++ src/MyList.js
@@ -3,9 +3,38 @@ import './App.css'
 import ListItem from './components/ListItem'
 
 class MyList extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      toDoItemArray: props.theList,
+      newItem: ""
+    }
+  }
+
+  clearList (e) {
+    console.log("Clearing list!")
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+  newItemChange (e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem (e) {
+   e.preventDefault()
+   let newArray = this.state.toDoItemArray
+   newArray.push(this.state.newItem)
+   this.setState({
+    toDoItemArray: newArray,
+    newItem: ''
+   })
+  }
   
   render() {
-    let todoItems = this.props.theList.map((item, index) => (
+    let todoItems = this.state.toDoItemArray.map((item, index) => (
       <ListItem doThis={item} key={index} />
     ))
     return (
@@ -15,6 +44,15 @@ class MyList extends Component {
         <ul>
          {todoItems}
         </ul>
+        <form>
+          <input type="text"
+          placeholder="Add new todo list item"
+          onChange={(e) => this.newItemChange(e)}
+          value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
+        <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
         </div>
       </div>
     )
